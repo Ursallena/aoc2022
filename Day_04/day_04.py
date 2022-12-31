5f2c2800dfbe1d0666ed8421d186dc7b44a3040d
Phillip Pring <phillip.pring@convergint.com>
2022-12-31 19:20:43 +1100

    Correct solution and data

--- Day_04/day_04.py
+++ Day_04/day_04.py
@@ -1,12 +1,3 @@
-def format_elf(e):
-	if e[1] < e[0]:
-		temp = e[0]
-		e[0] = e[1]
-		e[1] = temp
-		return e
-	else:
-		return e
-
 def compare_elfs(e1, e2):
 	if e1[0] >= e2[0] and e1[1] <= e2[1]:
 		return True
@@ -21,10 +12,9 @@ for line in open("data.txt", "r"):
 	if "\n" == line[-1]:
 		line = line[:-1]
 
-
 	line = line.split(",")
-	first_elf = format_elf(line[0].split("-"))
-	second_elf = format_elf(line[1].split("-"))
+	first_elf = line[0].split("-")
+	second_elf = line[1].split("-")
 	first_elf[0] = int(first_elf[0])
 	first_elf[1] = int(first_elf[1])
 	second_elf[0] = int(second_elf[0])
