d354581100447bb4e66d94da7324a04b8e5a7e5a
Phillip Pring <phillip.pring@convergint.com>
2022-12-31 19:21:42 +1100

    Part two!

---
+++ Day_04/day_04_part_02.py
@@ -0,0 +1,27 @@
+def compare_elfs(e1, e2):
+	if e1[0] >= e2[0] and e1[1] <= e2[1]:
+		return True
+	elif e1[0] <= e2[0] and e1[1] >= e2[1]:
+		return True
+	else:
+		return False
+
+
+total_overlap = 0
+for line in open("data.txt", "r"):
+	if "\n" == line[-1]:
+		line = line[:-1]
+
+	line = line.split(",")
+	first_elf = line[0].split("-")
+	second_elf = line[1].split("-")
+	first_elf[0] = int(first_elf[0])
+	first_elf[1] = int(first_elf[1])
+	second_elf[0] = int(second_elf[0])
+	second_elf[1] = int(second_elf[1])
+
+	if compare_elfs(first_elf, second_elf):
+		total_overlap += 1
+
+
+print(total_overlap)
