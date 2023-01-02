d269c6d218cc20ba374854269ef6103a354aac7d
Phillip Pring <phillip.pring@convergint.com>
2023-01-03 09:00:26 +1100

    Day 5

--- Day_05/day_05.py
+++ Day_05/day_05.py
@@ -1,1 +1,17 @@
-hello! 
+class column:
+	def __init__(self):
+		self.data = []
+
+
+column_list = []
+for i in range(0, 9):
+	column_list.append(column())
+
+for line in open("data.txt", "r"):
+	row = []
+	for i in range(0, (len(line) // 4)):
+		row.append(line[i * 4:i * 4 + 3])
+
+	print(row)
+
+
