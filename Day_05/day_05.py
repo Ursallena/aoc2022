a4860921183b107fb4719282207cc9780bbdc028
Phillip Pring <phillip.pring@convergint.com>
2023-01-03 09:27:37 +1100

    Day 5 complete! Added Day 6

--- Day_05/day_05.py
+++ Day_05/day_05.py
@@ -2,16 +2,43 @@ class column:
 	def __init__(self):
 		self.data = []
 
+	def put_on_top(self, container):
+		self.data.append(container)
+
+	def put_on_bottom(self, container):
+		self.data.insert(0, container)
+
+	def remove_from_top(self):
+		temp = self.data[-1]
+		self.data.pop()
+		return temp
+
+	def return_data(self):
+		return self.data
+
+	def return_top_container(self):
+		return self.data[-1]
+
 
 column_list = []
 for i in range(0, 9):
 	column_list.append(column())
 
 for line in open("data.txt", "r"):
-	row = []
-	for i in range(0, (len(line) // 4)):
-		row.append(line[i * 4:i * 4 + 3])
+	if "[" in line:
+		for i in range(0, (len(line) // 4)):
+			temp = line[i * 4:i * 4 + 3]
 
-	print(row)
+			if temp[0] == "[":
+				column_list[i].put_on_bottom(temp)
 
+
+	if line[0] == "m":
+		temp = line.strip().split(" ")
+		for i  in range(0, int(temp[1])):
+			column_list[int(temp[-1]) - 1].put_on_top(column_list[int(temp[3]) - 1].remove_from_top())
+
+
+for column in column_list:
+	print(column.return_top_container())
 
