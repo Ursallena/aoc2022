84fc8bfa0b54938c6af74fa6ed30cc4352c19a6c
Phillip Pring <emailsforpip@gmail.com>
2023-02-01 09:22:12 +1100

    working on comparing at the moment, will finish later

--- Day_13/day_13_attempt_two.py
+++ Day_13/day_13_attempt_two.py
@@ -7,33 +7,92 @@ ord_list = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
 
 # Actually in python there's a really easy way to parse the input...
 # ...perhaps you shoudl EVALuate your options...
+## I'm committed to making things difficult for myself, so it stays!
 def format_input(line_input):	
 	temp = []
 	list_depth = 0
+	long_int = False
 
-	for c in line_input:
+	for i in range(0, len(line_input)):
+		c = line_input[i]
 		if c == "[":
 			temp.append([])
 			in_last_list = True
 			list_depth += 1
 
-		elif ord(c) in ord_list:
-			c = int(c) # you know the input can have multi-digit numbers right?
-			temp[-1].append(c)
+		elif ord(c) in ord_list and long_int == False:
+			temp_int = ""
+			loop_index = 1
+			while not c in [",", "]"]:
+				temp_int += c
+				c = line_input[i + loop_index]
+				loop_index += 1
+
+			long_int = True
+			temp[-1].append(int(temp_int))
 
 		elif c == "]" and list_depth >= 2:
 			temp[-2].append(temp[-1])
 			temp.pop()
 			list_depth -= 1
+			long_int = False
+
+		elif c == ",":
+			long_int = False
 
 	return temp
+
+def compare_lists(left, right):
+	list_type = type([])
+	int_type = type(0)
+	temp = "Null"
+
+	if left == [] and right == []:
+		return "Null"
+
+	elif left == [] and not right == []:
+		return True
+
+	elif not left == [] and right == []:
+		return False
+
+	while not left == [] and not right == []:
+		left_type = type(left[0])
+		right_type = type(right[0])
+
+		if left_type == list_type and right_type == list_type:
+			temp = compare_lists(left[0], right[0])
+			
+		elif left_type == list_type and right_type == int_type:
+			temp = compare_lists(left[0], [right[0]])
+
+		elif left_type == int_type and right_type == list_type:
+			temp = compare_lists([left[0]], right[0])
+
+		else: #Should only get to this point if both are ints
+			if left[0] < right[0]:
+				return True
+
+			elif left[0] > right[0]:
+				return False
+
+			else:
+				return "Null"
+
+
+		if not temp == "Null":
+				return temp
+
+	return "Null"
+
+
+
 
 def compare_sides(left, right):
 	left = format_input(left)
 	right = format_input(right)
 	
-	
-
+	return compare_lists(left, right)
 	
 
 left = False
